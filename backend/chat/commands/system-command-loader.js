d5f6a70340bf83adc39b7fa7739206f02d2851c9
CaveMobster <cavemobster@gmail.com>
2022-01-21 11:07:10 +0100

    Update the system commands loader

--- backend/chat/commands/system-command-loader.js
+++ backend/chat/commands/system-command-loader.js
@@ -3,28 +3,21 @@
 exports.loadCommands = () => {
     const systemCommandManager = require("./system-command-manager");
 
-    // get command definitions
-    const commandList = require("./builtin/commandList");
-    const commandManagement = require("./builtin/commandManagement");
-    const uptime = require("./builtin/uptime");
-    const followage = require("./builtin/followage");
-    const quotesManagement = require('./builtin/quotes');
+    [
+        'steam/steam',
+        'command-list',
+        'command-management',
+        'custom-role-management',
+        'followage',
+        'marker',
+        'quotes',
+        'spam-raid-protection',
+        'uptime'
+    ].forEach(filename => {
+        const definition = require(`./builtin/${filename}.js`);
+        systemCommandManager.registerGame(definition);
+    });
+
     const currencyManager = require('../../currency/currencyManager');
-    const steam = require("./builtin/steam/steam");
-    const customRoleManagement = require("./builtin/custom-role-management");
-    const marker = require('./builtin/marker');
-    const spamRaidProtection = require('./builtin/spam-raid-protection');
-
-    // register them
-    systemCommandManager.registerSystemCommand(commandList);
-    systemCommandManager.registerSystemCommand(commandManagement);
-    systemCommandManager.registerSystemCommand(uptime);
-    systemCommandManager.registerSystemCommand(followage);
-    systemCommandManager.registerSystemCommand(quotesManagement);
-    systemCommandManager.registerSystemCommand(steam);
-    systemCommandManager.registerSystemCommand(customRoleManagement);
-    systemCommandManager.registerSystemCommand(marker);
-    systemCommandManager.registerSystemCommand(spamRaidProtection);
-
     currencyManager.createAllCurrencyCommands();
 };
